95f2f78c5abcc3a2fc462b6b577cf466b5520585
Foysal <mohibullah2k0@gmail.com>
2026-01-31 12:24:21 +0600

    feat: Update workmanager dependency to version 0.6.0 and set minSdkVersion to flutter.minSdkVersion for compatibility

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -24,7 +24,7 @@ android {
         applicationId = "com.example.rescunetapp"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
-        minSdk = 21  // Explicitly set for sqflite compatibility
+        minSdk = flutter.minSdkVersion  // Explicitly set for sqflite compatibility
         targetSdk = 34
         versionCode = 2
         versionName = "1.0.1"
